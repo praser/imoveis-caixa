47dc07832f81e98a20a3be0978677ef26eafd4a3
Rubens Praser Júnior <praser@gmail.com>
2018-08-06 21:46:09 -0300

    Criation of the script

--- Gemfile
+++ Gemfile
@@ -4,7 +4,5 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
 gem 'httparty'
-gem 'rubyzip'
 gem 'nokogiri'
---
+++ script.rb
@@ -0,0 +1,49 @@
+require 'httparty'
+require 'nokogiri'
+
+# Download files from server
+ufs = %w(AC AL AP AM BA CE DF ES GO MA MT MS MG PA PB PR PE PI RJ RN RS RO RR SC SP SE TO)
+ufs.each do |uf|
+  filename = "Lista_imoveis_#{uf}"
+  url = "http://www1.caixa.gov.br/listaweb/#{filename}.zip"
+  p "Downloading #{filename}"
+  
+  File.open("./files/#{filename}.7z", "w") do |file|
+    HTTParty.get(url, stream_body: true) do |fragment|
+      file.write(fragment)
+    end
+  end
+end
+
+# Uncompress files
+system "7za -y x './files/*.7z' -o./files"
+
+# Parse files
+data = { realty: [] }
+files = Dir['./files/*.htm']
+
+files.each do |file|
+  p "Processing #{file}"
+  doc = File.open(file) { |f| Nokogiri::HTML(f) }
+  doc.css("html > body > table > tr:nth-child(n+2)").each do |line|
+    data[:realty].push({
+      address: line.at_css("td:nth-child(2) > p > span").text.strip.squeeze(" ").gsub("\n", ''),
+      neighborhood: line.at_css("td:nth-child(3) > p > span").text.strip.squeeze(" ").gsub("\n", ''),
+      city: line.at_css("td:nth-child(7) > p > span").text.strip.squeeze(" ").gsub("\n", ''),
+      state: line.at_css("td:nth-child(8) > p").text.strip.squeeze(" ").gsub("\n", ''),
+      description: line.at_css("td:nth-child(4) > p > span").text.strip.squeeze(" ").gsub("\n", ''),
+      has_photo: line.at_css("td:nth-child(6) > p > u > span > a").text.strip.downcase === "ver foto",
+      price: line.at_css("td:nth-child(5) > p > span").text.strip.squeeze(" ").gsub("\n", ''),
+      url: line.at_css("td:nth-child(1) > p > u > span > a").attr('href')
+    })
+  end
+end
+
+# Write to output
+File.open("./files/#{Time.now.strftime '%Y-%m-%dT%H:%M:%S'}-data.json", 'w') do |file|
+  file.write(data.to_json)
+end
+
+# Clear directory
+system 'rm -rf ./files/*.7z'
+system 'rm -rf ./files/*.htm'
