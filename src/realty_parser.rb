ed2abac00bcd6b31fe9730db7023ac94119de34f
Rubens Praser Júnior <praser@gmail.com>
2018-08-09 02:36:01 -0300

    refactoring and bug fixing

--- src/realty_parser.rb
+++ src/realty_parser.rb
@@ -9,7 +9,7 @@ class RealtyParser
 
   def parse
     @files.each do |file|
-      @data[:realty].push parse_file(file)
+      parse_file(file)
     end
 
     write_to_file(@data)
@@ -20,27 +20,59 @@ class RealtyParser
   attr :data, :files, :path
 
   def parse_file(file)
-    obj = nil
     doc = File.open(file) { |f| Nokogiri::HTML(f) }
     doc.css("html > body > table > tr:nth-child(n+2)").each do |line|
-      address = trim line.at_css("td:nth-child(2) > p > span").text
-      neighborhood = trim line.at_css("td:nth-child(3) > p > span").text
-      city = trim line.at_css("td:nth-child(7) > p > span").text
-      state = trim line.at_css("td:nth-child(8) > p").text
-      description = trim line.at_css("td:nth-child(4) > p > span").text
-      has_photo = line.at_css("td:nth-child(6) > p > u > span > a").text.strip.downcase === "ver foto"
-      price = (trim line.at_css("td:nth-child(5) > p > span").text.gsub('.', '').gsub(',', '.')).to_f
-      url = line.at_css("td:nth-child(1) > p > u > span > a").attr('href').strip
-
-      obj = set_realty(address, neighborhood, city, state, description, has_photo, price, url)
+      @data[:realty].push set_realty(
+        address(line),
+        neighborhood(line),
+        city(line),
+        state(line),
+        description(line),
+        has_photo(line),
+        price(line),
+        url(line)
+      )
     end
     
-    return obj
+    return @data
   end
 
   def trim(text)
     text.strip.squeeze(" ").gsub("\n", '')
   end
+
+  def address(line)
+    trim line.at_css("td:nth-child(2) > p > span").text
+  end
+
+  def neighborhood(line)
+    trim line.at_css("td:nth-child(3) > p > span").text
+  end
+
+  def city(line)
+    trim line.at_css("td:nth-child(7) > p > span").text
+  end
+
+  def state(line)
+    trim line.at_css("td:nth-child(8) > p").text
+  end
+
+  def description(line)
+    trim line.at_css("td:nth-child(4) > p > span").text
+  end
+
+  def has_photo(line)
+    line.at_css("td:nth-child(6) > p > u > span > a").text.strip.downcase === "ver foto"
+  end
+
+  def price(line)
+    (trim line.at_css("td:nth-child(5) > p > span").text.gsub('.', '').gsub(',', '.')).to_f
+  end
+
+  def url(line)
+    line.at_css("td:nth-child(1) > p > u > span > a").attr('href').strip
+  end
+
 
   def set_realty(address, neighborhood, city, state, description, has_photo, price, url)
     return {
